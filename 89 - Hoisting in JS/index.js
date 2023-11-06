a7ac9a3619b9c60b71d9e8eadb6a0c70a9a4b90e
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2023-11-06 20:15:41 +0530

    Update index.js

--- 89 - Hoisting in JS/index.js
+++ 89 - Hoisting in JS/index.js
@@ -6,5 +6,5 @@ var greet = function() {
   console.log("Good morning")
 }
 
-var a = 9; // Declaration hoisted to the top but initialization is not
+var b = 9; // Declaration hoisted to the top but initialization is not
 console.log(a)
