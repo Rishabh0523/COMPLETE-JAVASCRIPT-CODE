72dfcdcb55b1650ae74ffe0bddd79778218c8847
Rishabh <76212232+Rishabh0523@users.noreply.github.com>
2023-12-01 21:16:42 +0530

    Update index.js

--- 04 - Primitive Object/index.js
+++ 04 - Primitive Object/index.js
@@ -1,9 +1,9 @@
 // nn bb ss u - Primitives in Js
 let a = null;
-let b = 345;
+let b = 350;
 let c = true; // can also be false
 let d = BigInt("567") + BigInt("3")
-let e = "Harry"
+let e = "Rishabh"
 let f = Symbol("I am a nice symbol")
 let g
 console.log(a, b, c , d, e, f, g)
